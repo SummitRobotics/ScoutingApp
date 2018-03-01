2dab108de2a0506351b16248bfb6830923e21198
SummitRobotics <summithighschoolrobotics@gmail.com>
2018-02-28 16:27:05 -0800

    Basic setup, basicly no progress

---
+++ app/src/main/java/com/summitrobotics/testapp/DataObjects/Match.java
@@ -0,0 +1,9 @@
+package com.summitrobotics.testapp.DataObjects;
+
+/**
+ * Created by summitrobotics on 2/28/2018.
+ */
+
+public class Match {
+    
+}
---
+++ app/src/main/java/com/summitrobotics/testapp/DataObjects/Team.java
@@ -0,0 +1,18 @@
+package com.summitrobotics.testapp.DataObjects;
+
+/**
+ * Created by summitrobotics on 2/28/2018.
+ */
+
+public class Team {
+    private Integer teamNumber;
+
+    public Team(Integer teamNumber) {
+        this.teamNumber = teamNumber;
+    }
+
+    public Integer getTeamNumber() {
+        return teamNumber;
+    }
+
+}
